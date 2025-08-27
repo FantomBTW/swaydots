b4af05e5bea287405c0a71d034633b257da6ee8e
Fantom <148335290+FantomBTW@users.noreply.github.com>
2025-08-27 13:54:21 +0000

    Add files via upload

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,8 +4,11 @@ local Plug = vim.fn['plug#']
 vim.call('plug#begin')
 
 vim.call('plug#', 'nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
+
 vim.call('plug#', 'sahlte/timed-highlight.nvim')
 vim.call('plug#', 'max397574/better-escape.nvim')
+--vim.call('plug#', 'girishji/vimcomplete')
+vim.call('plug#', 'nvim-lualine/lualine.nvim')
 
 vim.call('plug#', 'folke/tokyonight.nvim')
 vim.call('plug#', 'morhetz/gruvbox')
@@ -15,6 +18,8 @@ vim.call('plug#end')
 
 require"comm"
 
+
+
 -- Настройка Treesitter
 vim.cmd('syntax enable')
 vim.cmd('set background=dark')
@@ -23,16 +28,20 @@ vim.cmd('set background=dark')
 --vim.cmd('colorscheme catppuccin')
 vim.cmd('colorscheme gruvbox')
 
+require('tokyonight').setup({
+	style = "storm", -- storm day moon night
+})
+
+--прозрачность, наврное
+vim.cmd('hi Normal guibg=NONE ctermbg=NONE')
+
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = {"python","go","css"}, -- Установить поддерживаемые языки
+  ensure_installed = {"python","go","css","c","cpp"}, -- Установить поддерживаемые языки
   highlight = {
     enable = true,
   },
 }
 
-require('tokyonight').setup({
-	style = "storm", -- storm day moon night
-})
 
 
 --подсветка
@@ -45,3 +54,11 @@ require"better_escape".setup{
 	timeout = 500 ,
    	shortcut = 'jj'  -- замените на нужную вам комбинацию
 }
+
+require('lualine').setup {
+    options = {
+        theme = 'auto', -- или любой другой подходящий вам
+    }
+}
+
+
