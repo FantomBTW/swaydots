43fe42d53aaa176acb6a02967086d94bbf8ca4bd
Fantom <148335290+FantomBTW@users.noreply.github.com>
2025-09-07 12:00:01 +0000

    deleted asla(was mistake)

--- pack.sh
+++ pack.sh
@@ -1,4 +1,4 @@
-sudo pacman -S sway swaybg swaync neovim swaylock mpv waybar alsa alsa-utils pulseaudio pulseaudio-alsa pulseaudio-bluetooth cliphist wl-clipboard wofi firefox kitty yazi htop slurp grim fish networkmanager base-devel xdg-desktop-portal xdg-desktop-portal-wlr xorg-xwayland telegram-desktop 7zip 
+sudo pacman -S sway swaybg swaync neovim swaylock mpv waybar alsa-utils pulseaudio pulseaudio-alsa pulseaudio-bluetooth cliphist wl-clipboard wofi firefox kitty yazi htop slurp grim fish networkmanager base-devel xdg-desktop-portal xdg-desktop-portal-wlr xorg-xwayland telegram-desktop 7zip 
 chsh -s $(which fish)
 git clone https://aur.archlinux.org/yay.git
 cd yay
