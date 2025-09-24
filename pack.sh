e54ec4e60cce973a1f0a1e0447100daf7abb67f7
Fantom <148335290+FantomBTW@users.noreply.github.com>
2025-09-24 08:08:01 +0300

    Update pack.sh

--- pack.sh
+++ pack.sh
@@ -1,9 +1,12 @@
-sudo pacman -S sway swaybg swaync neovim swaylock mpv waybar alsa-utils pulseaudio pulseaudio-alsa pulseaudio-bluetooth cliphist wl-clipboard wofi firefox kitty yazi htop slurp grim fish networkmanager base-devel xdg-desktop-portal xdg-desktop-portal-wlr xorg-xwayland telegram-desktop 7zip 
-chsh -s $(which fish)
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+if [ "$UID" -eq 0 ]; then
+   echo "никаких рут прав, чел"
+else
+   sudo pacman -S sway swaybg swaync neovim swaylock mpv waybar alsa-utils pulseaudio pulseaudio-alsa pulseaudio-bluetooth cliphist wl-clipboard wofi firefox kitty yazi htop slurp grim fish networkmanager base-devel xdg-desktop-portal xdg-desktop-portal-wlr xorg-xwayland telegram-desktop zip unzip
+   chsh -s $(which fish)
+   git clone https://aur.archlinux.org/yay.git
+   cd yay
+   makepkg -si
+   sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-yay -S bluetuith wps-office-bin 
-
+   yay -S bluetuith wps-office-bin 
+fi
